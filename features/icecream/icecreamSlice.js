d573c00f1360d079163f873bb2e5144c360e860e
Sathya Vakacharla <sathyavakacharla@192.168.1.107>
2023-03-07 17:30:02 +0000

    intoduced loggers, extra reducers, createAsyncThunk

--- features/icecream/icecreamSlice.js
+++ features/icecream/icecreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require("../cake/cakeSlice");
+
 const createSlice = require("@reduxjs/toolkit").createSlice
 
 const initialState = {
@@ -14,6 +16,16 @@ const icecreamSlice = createSlice({
         restocked: (state, action) => {
             state.numberOfIcecreams += action.payload;
         }
+    },
+    // extraReducers: {
+    //     ['cake/ordered'] : (state) => {
+    //         state.numberOfIcecreams--;
+    //     }
+    // }
+    extraReducers: (builder) => { 
+        builder.addCase(cakeActions.ordered, (state) => {
+            state.numberOfIcecreams--;
+        })
     }
 })
 
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const store = require('./app/store');
-
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions;
-
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 console.log('Initial State', store.getState());
 
-const subscribe = store.subscribe(() => {
-    console.log('Updated state', store.getState());
-})
+const subscribe = store.subscribe(() => {})
 
+store.dispatch(fetchUsers());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
